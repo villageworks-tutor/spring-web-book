c55a579e89898a3145fa2d9293a557892bd7255a
villageworks <villageworks@gmail.com>
2023-12-18 23:01:02 +0900

    一時的な変更の保存

---
+++ src/main/java/com/example/demo/SpringWebBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebBookApplication.class, args);
+	}
+
+}
